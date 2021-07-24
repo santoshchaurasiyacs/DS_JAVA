2272dbb58a42c68c160ed8a40ae64edb0323c09d
santosh kumar <santosh.cse17@gmail.com>
2021-07-24 12:36:31 +0530

    first commit

---
+++ Factors.java
@@ -0,0 +1,15 @@
+public class Factors {
+    public static void main(String[] args) {
+        int n = 11;
+        int div = 1;
+        System.out.println("Factor of " +n);
+        while(div<=n){
+            if(n%div == 0){
+                 System.out.println(div);
+            }
+        div = div+1;   
+        }
+        // System.out.println(div);
+        
+    }
+}
